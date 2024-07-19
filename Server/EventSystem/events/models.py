326d410c700d20667f30a45e745257f88b5ff3c5
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-07-19 19:11:50 +0300

    Creating Models

--- Server/EventSystem/events/models.py
+++ Server/EventSystem/events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+    attendees = models.ManyToManyField(User, related_name='attendees')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
+    
+    def __str__(self):
+        return self.name
+    
+class Booking(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='events_bookings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_bookings')
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.user.username} booking for {self.event.name}'
