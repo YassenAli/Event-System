9229362cc80f6e9e73fd5930a6b63cbb032691c0
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-08-16 20:52:35 +0300

    solve problems with api

--- Client/src/Pages/Admin/Manage User/AddUser.js
+++ Client/src/Pages/Admin/Manage User/AddUser.js
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import '../../../App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Alert from 'react-bootstrap/Alert';
+import React, { useState, useEffect } from "react";
+import "../../../App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Alert from "react-bootstrap/Alert";
 import axios from "axios";
-import { getAuthUser } from "../../../helper/Storage";
+import { getAuthUser, getAccessToken, getRefreshToken } from "../../../helper/Storage";
 
 export default function AddUser() {
   const auth = getAuthUser();
-  
-  const [userData, setUserData] = useState({ 
-    username: '', 
-    email: '', 
-    password: '',
+
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
     err: "",
     success: null,
-    reload:false,
-    loading: false, 
+    reload: false,
+    loading: false,
   });
-
 
   // const [isEditing, setIsEditing] = useState(false);
   // const [editUserId, setEditUserId] = useState(null);
 
   useEffect(() => {
     setUserData({ ...userData, loading: true });
-    axios.get("/api/signup/")
+    axios
+      .get("http://127.0.0.1:8000/api/users")
       .then((resp) => {
         setUserData({ ...userData, results: resp.data, loading: false });
       })
@@ -39,49 +39,44 @@ export default function AddUser() {
       });
   }, [userData.reload]);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
 
-  // const handleChange = (e) => {
-  //   const { name, value } = e.target;
-  //   setUserData({ ...userData, [name]: value });
-  // };
-
-    //CREATE EVENT ON SUBMIT 
-    const createUser = (e) => {
-      e.preventDefault();
-      setUserData({ ...userData, loading: true });
-      
-
-      const token = localStorage.getItem('token');
-
-      const formData = new formData();
-      formData.append("username", userData.username);
-      formData.append("email", userData.email);
-      formData.append("password", userData.password);
-      axios.post("/api/signup/", formData, {
-        headers: {
-          Authorization: `Bearer ${token}`,
+  //CREATE EVENT ON SUBMIT
+  const createUser = (e) => {
+    e.preventDefault();
+    setUserData({ ...userData, loading: true });
+    const formData = new FormData();
+    formData.append("username", userData.username);
+    formData.append("email", userData.email);
+    formData.append("password", userData.password);
+    axios
+      .post("http://127.0.0.1:8000/api/signup/", formData, {
+        header: {
+          Authorization: `Bearer ${getAccessToken()}`,
         },
-        })
-        .then((resp) => {
-          setUserData({ ...userData, success: "User Created Successfully" });
-        })
-        .catch((err) => {
-          setUserData({
-            ...userData,
-            success: null,
-            err: "some thing went wrong, please try again later!",
-          });
+      })
+      .then((resp) => {
+        setUserData({ ...userData, success: "User Created Successfully" });
+      })
+      .catch((err) => {
+        setUserData({
+          ...userData,
+          success: null,
+          err: "some thing went wrong, please try again later!",
         });
-    };
-
+      });
+  };
 
   return (
     <div className="manage-user-container">
       <div className="add-nav">
-        <h3 style={{fontSize:"23px"}}>Add User</h3>
+        <h3 style={{ fontSize: "23px" }}>Add User</h3>
       </div>
       <form onSubmit={createUser} className="manage-user-form">
-      {userData.err && (
+        {userData.err && (
           <Alert variant={"danger"} className="auth-alert w-100">
             {userData.err}
           </Alert>
@@ -91,85 +86,61 @@ export default function AddUser() {
             {userData.success}
           </Alert>
         )}
-      <Container>
-      <Row>
-        <Col sm={11}>
-        <Row>
-        <input
-          type="text"
-          name="username"
-          value={userData.username}
-          onChange={(e) => setEventData({...userData, username: e.target.value})} //onChange={(e) => setEventData({...userData, username: e.target.value})}
-          placeholder="Username"
-          required
-          className="manage-user-input"
-        />
-        </Row>
-        <Row>
-        <input
-          type="email"
-          name="email"
-          value={userData.email}
-          onChange={(e) => setEventData({...userData, email: e.target.value})}
-          placeholder="Email"
-          required
-          className="manage-user-input"
-        />
-        </Row>
-        <Row>
-        <input
-          type="password"
-          name="password"
-          value={userData.password}
-          onChange={(e) => setEventData({...userData, password: e.target.value})}
-          placeholder="Password"
-          required
-          className="manage-user-input"
-        />
-        </Row>
-        </Col>
-        <Col sm={1}>
-	      <button type="submit" className="manage-user-button">
-          Create User
-        </button>
-        </Col>
-      </Row>
-    </Container>
+        <Container>
+          <Row>
+            <Col sm={11}>
+              <Row>
+                <input
+                  type="text"
+                  name="username"
+                  value={userData.username}
+                  onChange={handleChange}
+                  placeholder="Username"
+                  required
+                  className="manage-user-input"
+                />
+              </Row>
+              <Row>
+                <input
+                  type="email"
+                  name="email"
+                  value={userData.email}
+                  onChange={handleChange}
+                  placeholder="Email"
+                  required
+                  className="manage-user-input"
+                />
+              </Row>
+              <Row>
+                <input
+                  type="password"
+                  name="password"
+                  value={userData.password}
+                  onChange={handleChange}
+                  placeholder="Password"
+                  required
+                  className="manage-user-input"
+                />
+              </Row>
+            </Col>
+            <Col sm={1}>
+              <button type="submit" className="manage-user-button">
+                {/* {isEditing ? 'Update User' : 'Add'} */}
+                Create User
+              </button>
+            </Col>
+          </Row>
+        </Container>
       </form>
+      {/* <div className="user-list">
+        {users.map((user) => (
+          <div key={user._id} className="user-list-item">
+            <span>{user.username} ({user.email})</span>
+            <button onClick={() => handleEdit(user)} className="edit-button">Edit</button>
+            <button onClick={() => handleDelete(user._id)} className="delete-button">Delete</button>
+          </div>
+        ))}
+      </div> */}
     </div>
-  );
+  );
 }
-
-
-
-
-/* Backend */
-
-// // Create a new user
-// app.post('/api/signup/', async (req, res) => {
-//   const { username, email, password } = req.body;
-//   const newUser = new User({ username, email, password });
-//   await newUser.save();
-//   res.status(201).json(newUser);
-// });
-
-// // Get all users
-// app.get('/api/signup/', async (req, res) => {
-//   const users = await User.find();
-//   res.status(200).json(users);
-// });
-
-// // Update a user
-// app.put('/api/signup//:id', async (req, res) => {
-//   const { id } = req.params;
-//   const { username, email, password } = req.body;
-//   const updatedUser = await User.findByIdAndUpdate(id, { username, email, password }, { new: true });
-//   res.status(200).json(updatedUser);
-// });
-
-// // Delete a user
-// app.delete('/api/signup//:id', async (req, res) => {
-//   const { id } = req.params;
-//   await User.findByIdAndDelete(id);
-//   res.status(204).send();
-// });
--- Client/src/Pages/Home.js
+++ Client/src/Pages/Home.js
@@ -24,7 +24,7 @@ export default function Events() {
 
   useEffect(() => {
     setEvents({ ...events, loading: true });
-    axios.get("/api/events", {
+    axios.get("http://127.0.0.1:8000/api/events/", {
       params: {
         search: search,
       }
@@ -43,7 +43,7 @@ export default function Events() {
 
   const fetchBookings = async () => {
     try {
-      const response = await axios.get(`/api/bookings`, {
+      const response = await axios.get(`http://127.0.0.1:8000/api/bookings`, {
         params: { email: userEmail }
       });
       setBookings(response.data);
@@ -54,7 +54,7 @@ export default function Events() {
 
   const handleBook = async (eventId) => {
     try {
-      const response = await axios.post('/api/bookings', {
+      const response = await axios.post('http://127.0.0.1:8000/api/bookings', {
         eventId,
         email: userEmail,
       });
@@ -73,7 +73,7 @@ export default function Events() {
     try {
       const booking = bookings.find(booking => booking.eventId === eventId);
       if (booking) {
-        const response = await axios.delete(`/api/bookings/${booking._id}`);
+        const response = await axios.delete(`http://127.0.0.1:8000/api/bookings/${booking._id}`);
 
         if (response.status === 200) {
           fetchBookings();
@@ -162,180 +162,11 @@ export default function Events() {
         !events.loading && !events.err && events.results.length === 0 && (
           <div className="d-flex justify-content-center align-items-center">
             <Alert variant="info" className="auth-alert text-center w-50">
-              There Is No Events 🎟️!
+              There Is No Events 🎟!
             </Alert>
           </div>
         )
       }
     </div>
-  );
+  );
 }
-
-
-// import React, { useState, useEffect } from 'react';
-// import '../../App.css';
-// import axios from "axios";
-// import Loader from './../../Components/Loader';
-// import Ticket from '../../Components/Ticket';
-// import Alert from 'react-bootstrap/Alert';
-// import { Form, FormControl, Button } from 'react-bootstrap';
-// import { getAuthUser } from "../../helper/Storage";
-
-// export default function Events() {
-//   const auth = getAuthUser();
-
-//   const [events, setEvents] = useState({
-//     loading: true,
-//     results: [],
-//     err: null,
-//     reload: 0,
-//   });
-//   const [search, setSearch] = useState('');
-
-//   const [userEmail, setUserEmail] = useState(auth?.email || '');
-//   const [bookings, setBookings] = useState([]);
-
-//   useEffect(() => {
-//     setEvents({ ...events, loading: true });
-//     axios.get("/api/events", {
-//       params: {
-//         search: search,
-//       }
-//     }).then((resp) => {
-//       setEvents({ ...events, results: resp.data, loading: false });
-//     }).catch((err) => {
-//       setEvents({ ...events, loading: false, err: 'Something went wrong, please try again later!' });
-//     });
-//   }, [events.reload]);
-
-//   useEffect(() => {
-//     if (auth) {
-//       fetchBookings();
-//     }
-//   }, [auth]);
-
-//   const fetchBookings = async () => {
-//     try {
-//       const response = await axios.get(`/api/bookings`, {
-//         params: { email: userEmail }
-//       });
-//       setBookings(response.data);
-//     } catch (error) {
-//       console.error('Error fetching bookings:', error);
-//     }
-//   };
-
-//   const handleBook = async (eventId) => {
-//     try {
-//       const response = await axios.post('/api/bookings', {
-//         eventId,
-//         email: userEmail,
-//       });
-
-//       if (response.status === 200) {
-//         fetchBookings();
-//       } else {
-//         console.error('Error booking event');
-//       }
-//     } catch (error) {
-//       console.error('Error booking event:', error);
-//     }
-//   };
-
-//   const handleCancel = async (bookingId) => {
-//     try {
-//       const response = await axios.delete(`/api/bookings/${bookingId}`);
-
-//       if (response.status === 200) {
-//         fetchBookings();
-//       } else {
-//         console.error('Error canceling booking');
-//       }
-//     } catch (error) {
-//       console.error('Error canceling booking:', error);
-//     }
-//   };
-
-//   const searchEvents = (e) => {
-//     e.preventDefault();
-//     setEvents({ ...events, reload: events.reload + 1 });
-//   };
-
-//   return (
-//     <div>
-//       {/* Loader */}
-//       {
-//         events.loading && <Loader />
-//       }
-
-//       {/* List Events */}
-//       {
-//         !events.loading && !events.err && (
-//           <>
-//             <div className="top-main-head d-flex justify-content-between mb-3" style={{ backgroundColor: "#efe8d8" }}>
-//               <h3 style={{ fontSize: "26px", color: "#524a37" }}>Events 🎫</h3>
-//               <Form className="d-flex" onSubmit={searchEvents}>
-//                 <FormControl
-//                   type="search"
-//                   placeholder="Search"
-//                   value={search}
-//                   className="me-2"
-//                   style={{ fontSize: '14px' }}
-//                   onChange={(e) => setSearch(e.target.value)}
-//                 />
-//                 <Button variant="outline-success" type="submit" style={{ fontSize: '14px' }}>Search</Button>
-//               </Form>
-//             </div>
-//             <div className="event-list">
-//               {events.results.map((event) => (
-//                 <div key={event.id} className="event-list-item">
-//                   <Ticket
-//                     title={event.title}
-//                     date={event.date}
-//                     time={event.time}
-//                     seatNumber={event.seatNumber}
-//                     description={event.description}
-//                   />
-//                   {bookings.some(booking => booking.eventId === event.id) ? (
-//                     <button onClick={() => handleCancel(event.id)} className="cancel-button">Cancel Booking</button>
-//                   ) : (
-//                     <button onClick={() => handleBook(event.id)} className="book-button">Book</button>
-//                   )}
-//                 </div>
-//               ))}
-//             </div>
-//           </>
-//         )
-//       }
-
-//       {/* Errors Handling */}
-//       {
-//         !events.loading && events.err && (
-//           <div className="d-flex justify-content-center align-items-center">
-//             <Alert variant="danger" className="auth-alert text-center w-50">
-//               {events.err}
-//             </Alert>
-//           </div>
-//         )
-//       }
-
-//       {!auth && (
-//         <div className="d-flex justify-content-center align-items-center">
-//           <Alert variant="warning" className="auth-alert text-center w-50">
-//             Please, login first to be able to book!
-//           </Alert>
-//         </div>
-//       )}
-
-//       {
-//         !events.loading && !events.err && events.results.length === 0 && (
-//           <div className="d-flex justify-content-center align-items-center">
-//             <Alert variant="info" className="auth-alert text-center w-50">
-//               There Is No Events 🎟️!
-//             </Alert>
-//           </div>
-//         )
-//       }
-//     </div>
-//   );
-// }
